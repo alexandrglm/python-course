0bf3894aa504094744e87a39e93c3f3990072e3c
Alexandr Gomez <undefined.platform159@passinbox.com>
2025-02-06 15:32:13 +0100

    Added Stringg basics:  * Quotation types  * Multiple quotations on a string

---
+++ 05-Strings_basics.py
@@ -0,0 +1,15 @@
+sentence = "The quick brown fox jumped over the lazy dog"
+
+"""
+sentence = "The quick brown fox jumped over the lazy dog'
+
+Never, never, try to merge both quotation types in the same object
+"""
+
+sentence_two = 'That\'s my dog\'s bowl'
+
+sentence_three = "That's my dog's bowl"
+
+sentence_four = 'Tiffany said, \"That\'s my dog\'s bowl"'
+
+print(sentence, sentence_two, sentence_three, sentence_four)
