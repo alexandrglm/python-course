310e9d062ea551bb9f8289b439b488140fde7e7c
Alexandr Gomez <alexandrglm@@users.noreply.github.com>
2025-02-06 12:31:03 +0100

    Updated to 02 - 018 refactoring Var. values

---
+++ 03-RefactoringVariables.py
@@ -0,0 +1,21 @@
+# From Module 02 - 018
+## This is an example on how you can change var values
+
+# meal_Complete = True
+# sub_total = 100
+# tip = sub_total * 1/5
+# total = sub_total + tip
+
+# receipt = "Your total is " + str(total)
+
+# print(receipt)
+
+first = 'Springman'
+second = 'Bregman'
+third = 'Altuve'
+
+print(first)
+
+first = third 
+
+print(first)
