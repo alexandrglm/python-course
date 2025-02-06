d2fe244b4745b672cfd66c9729787a5789ab5ba2
Alexandr Gomez <alexandrglm@@users.noreply.github.com>
2025-02-06 19:02:06 +0100

    Updated to 02-025 Accessing string portions.

---
+++ 07-AccessingStringPortions.py
@@ -0,0 +1,29 @@
+# The quick brown fox jumped.
+
+# starter_sentence = 'The quick brown fox jumped'
+# print(starter_sentence[0])
+
+# starter_sentence[12] = 'A'
+# print(starter_sentence)
+
+# first = starter_sentence[0]
+# second = starter_sentence[1]
+# third = starter_sentence[2]
+
+# new_sentence = print("First: " + first + " | Second: " + second + " | Third:" + third)
+# print(new_sentence)
+# First: T | Second: h | Third:e
+###
+
+# starter_sentence = 'The quick brown fox jumped'
+
+# first_word = starter_sentence[0:2]
+# new_sentence = first_word
+# print(new_sentence)
+##
+
+starter_sentence = 'The quick brown fox jumped'
+
+new_sentence = 'Thy' + starter_sentence[:3]
+
+print(new_sentence)
