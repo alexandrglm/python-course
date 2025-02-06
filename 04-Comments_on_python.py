17477365b00b4a301aa8b086a7f5fe1168085323
Alexandr Gomez <undefined.platform159@passinbox.com>
2025-02-06 14:47:27 +0100

    Updated to 02-018: Python types of comments

---
+++ 04-Comments_on_python.py
@@ -0,0 +1,14 @@
+# i am a single line comment
+
+inline_Comment = 'I am a variable attached to an in-line comment' # I am the in-lined comment
+
+print(inline_Comment)
+
+"""
+I am a MultiLine comment:
+
+I can write anything here using as many lines as needed.
+
+It's wonderful when trying to document something.
+Also when adding TODO notes
+"""
