c953c8d9fac44d2ba0d605184aa8705160fb5fc8
Alexandr Gomez <undefined.platform159@passinbox.com>
2025-02-06 16:18:44 +0100

    Updated to 02-024: * String case methods * Differences invoking a function vs an object.

---
+++ 06-String_cases_Reserved_Methods.py
@@ -0,0 +1,29 @@
+# .upper method
+
+# sentence = "The quick brown fox jumped"
+# sentence -> variable
+# "The quick brown fox jumped" -> String
+
+sentence = 'The quick brown fox jumped'
+sentence_two = sentence.upper()
+
+# print(sentence)
+# print(sentence_two)
+
+# .capitalize method
+sentence_three = 'the quick brown fox jumped'.capitalize()
+
+# print(sentence_three)
+
+
+# .title method
+sentence_four = 'the quick brown fox jumped'.title()
+
+# print(sentence_four)
+
+
+
+# .lower method
+sentence_five = 'THE QUICK BROWN FOX JUMPED'
+
+print(sentence_five.lower())
