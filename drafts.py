2e0a75f9e4c86624bf9e02e806e74d7d9b6e9ab2
Alexandr Gomez <alexandrglm@@users.noreply.github.com>
2025-02-07 10:59:29 +0100

    Created 02-028: Lambda function, reduce() method.

---
+++ drafts.py
@@ -0,0 +1,14 @@
+# # lambda function
+
+# x = lambda a : a +10
+
+# print(x(5))
+
+# Usando lambda con strings
+## Además, lambda puede acceder a cualquier variable.
+
+a = "hola que tal"
+
+to_upper = lambda a : a.upper()
+
+print(to_upper(a))
