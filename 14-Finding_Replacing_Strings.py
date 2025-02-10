279562500f67890fe5d4a51d01dfe296e30a5505
Alexandr Gomez <undefined.platform159@passinbox.com>
2025-02-10 14:17:30 +0100

    Added 02-031 .replace() function

---
+++ 14-Finding_Replacing_Strings.py
@@ -0,0 +1,8 @@
+# 02 031: .replace() function / Finding and replacing String values
+
+sentence = 'The quick brown fox jumped over the lazy dog'
+
+sentence = sentence.replace('fox', 'cat')
+# .replace('The value you are looking for', 'the desired value to be replaced')
+
+print(sentence)
