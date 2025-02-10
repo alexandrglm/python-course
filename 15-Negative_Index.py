8a191538e505dbe8fa2686b7e7c5dd21767a7be2
Alexandr Gomez <alexandrglm@@users.noreply.github.com>
2025-02-10 14:25:00 +0100

    Updated to 02- 033, Working with negative Negative index values and managing ranges.

---
+++ 15-Negative_Index.py
@@ -0,0 +1,14 @@
+# 02 - 033: Negative Index with a Python string
+
+sentence = 'The quick brown fox jumped over the lazy dog.'
+
+#  A Negative index allows to start by the end, and work with ranges
+
+# Syntax variable[StartFrom : Finish on]
+
+print(sentence[-4])
+
+# Managing ranges
+
+print(sentence[-4:]) # From -4 position until the end of the string
+print(sentence[-4:-2]) # From -4 to -2
