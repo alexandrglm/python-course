334f609befde36b762f54e4f96b3b96c379f1f89
Alexandr Gomez <alexandrglm@@users.noreply.github.com>
2025-02-11 18:56:46 +0100

    Started "Numbers". * 20 Numbers Overview (02-040) * 21 Mathematical Operators (02-041)

---
+++ 20-Numbers_overview.py
@@ -0,0 +1,14 @@
+# 02- 040: Numbers overview
+
+# product_id = 1234567890
+# sale_price = 14.99
+# tip_percentage = 1/5 # Same as 0.20, remember you can use fractions to represent decimals
+
+# print(tip_percentage)
+
+product_id = 1234567890
+sale_price = 14.99
+tip_percentage = 1/5
+new_product = 150
+
+print(sale_price + new_product)
---
+++ 31-Math_operators.py
@@ -0,0 +1,24 @@
+# 02- 041: mathematical operators
+print('Additions:')
+print(100 + 42)
+
+print('Substractions:')
+print(100 - 42)
+
+print('Divisions:')
+print(100 / 42)
+print(100 / 38)
+
+print('Floor Divisions:')
+print(100 // 42)
+print(100 // 38)
+
+print('Multiplications: ')
+print('--')
+
+print('Modulus (%): ')
+print(2 % 2)
+print(25 % 2)
+
+print('Exponents (^):')
+print(2 ** 3)
