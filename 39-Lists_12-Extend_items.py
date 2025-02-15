49a23528f1c5f506761eb8ccac2bb1d931d5e92d
Alexandr Gomez <alexandrglm@@users.noreply.github.com>
2025-02-15 18:11:05 +0100

    Updated to 02-060: Extending list items with `.extend()`, and also other simple methods.

--- 39-Lists_12-Extend_items.py
+++ 39-Lists_12-Extend_items.py
@@ -9,13 +9,33 @@ print('\nOriginal List:  ' + str(tags))
 tags[-1] = 'Programming (via [index])'
 print(tags)
 
+
 # Nope!
 # Extending via .extend()
-#tags.extend('Programming (via .extend())')
-# print(tags)     # our string has been inserted CHARACTER BY CHARACTER !
+tags.extend('Programming (via .extend())')
+print(tags)     # our string has been inserted CHARACTER BY CHARACTER !
 
 # Yep !
 # Extending via .extend( [ ] ) !
 
 tags.extend( ['Programming ( via .extend([ ]) )'] )    # Using ( [ ] ) !
 print(tags)     # ['python', 'development', 'tutorials', 'Programming (via [index])', 'Programming (via .extend( [ ] ) )']
+
+
+# Extending without using extend
+
+## Nope!
+## Uncomment this to understand the fully error.
+
+# new_tags = tags + 'programming' # Always WRAP into [Brackets]
+# print(new_tags)
+
+
+
+## Yep!
+new_tags = tags + ['programming']
+
+print(new_tags)
+
+
+print('\nOriginal List:  ' + str(tags))
