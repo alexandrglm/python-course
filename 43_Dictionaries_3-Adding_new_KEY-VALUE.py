1ed8570d135e39281c3bfb335264a19329308eff
Alexandr Gomez <alexandrglm@@users.noreply.github.com>
2025-02-17 17:05:43 +0100

    Updated to 02-065: `dictionary.get(key, value)`

---
+++ 43_Dictionaries_3-Adding_new_KEY-VALUE.py
@@ -0,0 +1,14 @@
+# 02-064: Adding new pair 'Key'/'Value' onto a dictionary
+
+teams = {
+    "astros"    : ["Altuve", "Correa", "Bregman"],
+    "angels"    : ["Trout", "Pujols"],
+    "yankees"   : ["Judge", "Stanton"]
+}
+
+
+# Adding
+# Syntax:   dictionarie['new key'] = ['new value', 'for the key']
+
+teams['red sox'] = ['Price', 'Betts']
+print(teams)
