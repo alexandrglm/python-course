d68a543b23453149f7a8a56b4da1ee9b61ebaed8
Alexandr Gomez <alexandrglm@@users.noreply.github.com>
2025-02-18 20:51:44 +0100

    Started 02-069 EXERCISE - Histogram

---
+++ 48-Dictionaries_8-EXERCISE_Histogram_without_ext-libraries.py
@@ -0,0 +1,32 @@
+# Module 02 - 069: Python: Dictionaries, Exercise:
+## Building an Histogram with no external libraries
+
+"""
+Concepts:
+
+g $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+f $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+x $$$$$$$$$$$$$$$$$
+o $$$$$$$$$$$$$$$$$$$$$$$$
+
+print(4 + 'w')  # NOPE!
+print(4 * 'w')  # YEP!
+
+"""
+
+# Attempt 1
+
+dict_1 = {
+    '$' :   '$' * 10
+}
+
+g = 7
+f = 8
+x = 4
+o = 5
+
+print( 'g:' + f'{dict_1['$'] * g}\n')
+print( 'f:' + f'{dict_1['$'] * f}\n')
+print( 'x:' + f'{dict_1['$'] * x}\n')
+print( 'o:' + f'{dict_1['$'] * o}\n')
+
