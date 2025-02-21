6f0b53323ca0a2a4e65c84bd4ec541d4cad828b6
Alexandr Gomez <alexandrglm@@users.noreply.github.com>
2025-02-21 16:35:09 +0100

    Added 03-083: Loops over ranges, stop off value.

---
+++ 62-LOOPs_4-Loop_over_Ranges.py
@@ -0,0 +1,23 @@
+# 03-082: Looping into Ranges
+
+# range()
+# Syntax: range(start, stop, step)
+# Ranges work similar to slicing, the STOP is off by one value!
+
+for number in range(1,10):
+    print(number)       # 1 2 3 4 5 6 7 8 9
+
+
+
+# Ranges work similar to slicing, the STOP is off by one value!
+for number in range(1,11):
+    print(number)       # 1 2 3 4 5 6 7 8 9 and .. 10 !
+
+
+# Stepping
+
+for number in range(1,11, 2):
+    print(number)       # 1 3 5 7 9
+
+for number in range(1,11, 3):
+    print(number)       # 1 4 7 10
