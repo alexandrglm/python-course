e43f570e9759e29f0d5113d43626a2671bde03dc
Alexandr Gomez <undefined.platform159@passinbox.com>
2025-02-21 13:04:35 +0100

    Started Control Structures: Loops. Added 03-080 for...in / while intro, examples.

---
+++ 59-LOOPs_1_Intro.py
@@ -0,0 +1,24 @@
+# 03-080. Control Structures -> Loops
+import random
+
+
+# for...in
+
+numbers = []
+
+for number in range(10):
+    numbers.append(str(random.randint(0, 10000)))
+
+print(numbers)
+
+
+
+# while 
+
+numbers_while = []
+
+while len(numbers_while) < 10:
+    do: numbers_while.append(str(random.randint(0, 1000)))
+
+print(numbers_while)
+
