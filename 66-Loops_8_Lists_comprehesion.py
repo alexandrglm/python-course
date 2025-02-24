ca8a21b4b94bdf743fe652c41690b430fea19614
Alexandr Gomez <undefined.platform159@passinbox.com>
2025-02-24 07:46:31 +0100

    Updated to 03-088, List comprehesion

---
+++ 66-Loops_8_Lists_comprehesion.py
@@ -0,0 +1,65 @@
+# 03-088: List comprehesion
+
+num_list = range(1,11)
+
+cubed_nums = []
+
+
+# A regular for...in expression
+
+for num in num_list:
+    cubed_nums.append(num ** 3)
+
+print(list(num_list))
+print(cubed_nums)   # [1, 8, 27, 64, 125, 216, 343, 512, 729, 1000]
+
+
+
+# A for...in declaration in a var
+
+cubed_nums = [num ** 3 for num in num_list]
+
+print(list(num_list))
+print(cubed_nums)       # Gives the same result
+
+
+# As we've seen before, the iterator can be named as wanted
+
+cubed_nums = [x ** 3 for x in num_list]
+
+print(list(num_list))
+print(cubed_nums)       # Gives the same result
+
+
+# Adding conditionals
+
+even_numbers = []
+
+for num in num_list:
+    if num % 2 == 0:
+        even_numbers.append(num)
+
+print(list(num_list))   # [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+print(even_numbers)     # [2, 4, 6, 8, 10]
+
+# In a expression
+
+even_numbers = [num for num in num_list if num % 2 == 0 ]
+
+print(list(num_list))   # [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+print(even_numbers)     # # [2, 4, 6, 8, 10]
+
+
+# Coding Exercise
+"""
+Create a variable called result and use list comprehension to increment each number from the numbers list by 1.
+"""
+
+"""
+def list_comprehension():
+    numbers = [1,2,3,4,5,6]
+    # Write your code here
+    
+    
+    return result
+"""
