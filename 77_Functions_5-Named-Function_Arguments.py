36c7fbf88d9e01df093905eea325f1c7428ddbc6
Alexandr Gomez <alexandrglm@@users.noreply.github.com>
2025-02-28 18:22:22 +0100

    Updated to #03-103, Keyword Arguments (**args)

--- 77_Functions_5-Named-Function_Arguments.py
+++ 77_Functions_5-Named-Function_Arguments.py
@@ -9,3 +9,11 @@ def full_name(first, last):
 full_name('Kristine', 'Hudgens')
 full_name(first = 'Kristine', last = 'Hudgens')
 full_name(last = 'Hudgens', first = 'Kristine')
+
+
+# CodingExercise
+"""
+Behind the scenes of the code test is a function called sequence that accepts 5 arguments: first, second, third, fourth, and fifth. Unfortunately, they are not in sequential order. Using named arguments, call the sequence function and pass in the 5 arguments, setting their values to 1, 2, 3, 4, 5 respectively.
+"""
+def named_arguments_practice(sequence):
+    sequence(first = 1, second = 2, third = 3, fourth = 4, fifth = 5)
